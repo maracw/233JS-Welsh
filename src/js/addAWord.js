82996a66ff6954f1276c56e78283b3ce644272a8
Mara's Mac <williamsmc@my.lanecc.edu>
2022-12-02 18:30:53 -0800

    working on clickable image choices

--- src/js/addAWord.js
+++ src/js/addAWord.js
@@ -22,20 +22,21 @@ class AddWord{
         this.pexels6Btn=document.getElementById("fetch-6pexels");
         this.pexelsSearch=document.getElementById("pexels-search");
         //bind methods to class
-        this.TestIConDisplay=this.TestIConDisplay.bind(this);
+        
         this.DownloadIcon=this.DownloadIcon.bind(this);
+        this.enableAllCards=this.enableAllCards.bind(this);
         //add methods to ui elements
         this.downloadBtn.onclick=this.DownloadIcon.bind(this);
         this.pexelsBtn.onclick=this.GetOnePexels.bind(this);
         this.pexels6Btn.onclick=this.GetSixPexels.bind(this);
-        this.TestIConDisplay();
+        
         
     }
     //onform change or indiv buttons
     /*submit should add the final*/
 
 
-    //method to fetch icon
+    //method to fetch one photo
     GetOnePexels(event){
         event.preventDefault();
         let search=this.pexelsSearch.value;
@@ -54,12 +55,12 @@ class AddWord{
             document.getElementById("fetch-img").src=this.state.photos[0].src.small;
         });
         }
-
+        //method to fetch 6 photos
         GetSixPexels(event){
             event.preventDefault();
             let search=this.pexelsSearch.value;
             let searchEncoded=encodeURIComponent(search);           
-            fetch(`https://api.pexels.com/v1/search?query=${searchEncoded}&size='small'&per_page=6`, {
+            fetch(`https://api.pexels.com/v1/search?query=${searchEncoded}&orientation='square'&size='small'&per_page=6`, {
                 headers: {
                     'Authorization': '563492ad6f91700001000001d4be53950c3d4ace99ac5c52efdf1558'
                 }
@@ -74,38 +75,51 @@ class AddWord{
             });
             this.DisplayPexels6();
         }
-    
-
+        //method to display 6 choices
         DisplayPexels6(){
             let htmlText="";
+            let idCount=0;
+            //this doesn't seem to work without the timeout?
             this.state.photos.forEach(element => {
+                idCount++;
                 let attribution=element.photographer;
                 console.log(attribution);
                 let imagePath=element.src.small;
                 let alt=element.alt;
                 htmlText+=`
-                <div id="result-img1" name="icon-choice" style="width: 175; height: 175;" class="p-2 bg-info">
-                <img src="${imagePath}" alt="${alt}" style="width: 200px; height: 200px;">
+                <div style="width: 175; height: 175;" class="p-2 bg-info">
+                <img name="icon-choice" id="result-img${idCount}" src="${imagePath}" alt="${alt}" style="width: 200px; height: 200px;">
                 <br> 
                 ${attribution}</div>`;
+                setTimeout(1500);
             });
             document.getElementById("display-choices").innerHTML=htmlText;
-            
+            this.enableAllCards();
         }
-    //disply downloaded default image in flexbox for testing
-    TestIConDisplay(){
-    let attribution='<a href="https://thenounproject.com/browse/icons/term/cat/" target="_blank" title="Cat Icons">Noun Project</a>';
-    let imagePath ="Assets/images/noun-cat-5328621.png";
-    let htmlText="";
-    for(let i=0;i<6;i++)
-    htmlText+=`
-        <div id="result-img1" name="icon-choice" style="width: 175; height: 175;" class="p-2 bg-info">
-        <img src="${imagePath}" style="width: 200px; height: 200px;">
-        <br> 
-        ${attribution}</div>`;
-    document.getElementById("display-choices").innerHTML=htmlText;
-    
-    }
+
+        //user clicks on card
+        //works like handleClick
+        ChosePhotoForCard(index){
+            let cardImage = this.state.photos[index].src.small;
+            let attribution=this.state.photos[index].attribution;
+            console.log(attribution);
+        }
+        handleClick(index) {
+            console.log(index);
+            let id="result-img"+(index+1);
+            let cardImage=document.getElementById(id).src;
+            document.getElementById("fetch-img").src=cardImage;
+        }
+
+        //makes photos clickable
+        enableAllCards() {
+            //watch out there is an onclick! bind!
+            let cards = document.getElementsByName("icon-choice");
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].onclick = this.handleClick.bind(this, i);
+                cards[i].style.cursor = 'pointer';
+            }
+        }
 
     DownloadIcon(event){
         event.preventDefault();
