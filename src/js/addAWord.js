c0144ed266788edfc8e819a43f89e45df95f9afd
Mara's Mac <williamsmc@my.lanecc.edu>
2022-12-02 17:35:50 -0800

    pexels search and display six works

--- src/js/addAWord.js
+++ src/js/addAWord.js
@@ -19,6 +19,7 @@ class AddWord{
         this.downloadBtn=document.getElementById("download-btn");
         this.iconUrlElement=document.getElementById("icon-download-url");
         this.pexelsBtn=document.getElementById("fetch-pexels");
+        this.pexels6Btn=document.getElementById("fetch-6pexels");
         this.pexelsSearch=document.getElementById("pexels-search");
         //bind methods to class
         this.TestIConDisplay=this.TestIConDisplay.bind(this);
@@ -26,6 +27,7 @@ class AddWord{
         //add methods to ui elements
         this.downloadBtn.onclick=this.DownloadIcon.bind(this);
         this.pexelsBtn.onclick=this.GetOnePexels.bind(this);
+        this.pexels6Btn.onclick=this.GetSixPexels.bind(this);
         this.TestIConDisplay();
         
     }
@@ -52,8 +54,44 @@ class AddWord{
             document.getElementById("fetch-img").src=this.state.photos[0].src.small;
         });
         }
+
+        GetSixPexels(event){
+            event.preventDefault();
+            let search=this.pexelsSearch.value;
+            let searchEncoded=encodeURIComponent(search);           
+            fetch(`https://api.pexels.com/v1/search?query=${searchEncoded}&size='small'&per_page=6`, {
+                headers: {
+                    'Authorization': '563492ad6f91700001000001d4be53950c3d4ace99ac5c52efdf1558'
+                }
+            })
+            .then(result=>result.json())
+            .then (photodata=>{
+                //do things
+                this.state.photos=photodata.photos;
+                console.log(this.state.photos);
+                //place pic
+                document.getElementById("fetch-img").src=this.state.photos[5].src.small;
+            });
+            this.DisplayPexels6();
+        }
     
 
+        DisplayPexels6(){
+            let htmlText="";
+            this.state.photos.forEach(element => {
+                let attribution=element.photographer;
+                console.log(attribution);
+                let imagePath=element.src.small;
+                let alt=element.alt;
+                htmlText+=`
+                <div id="result-img1" name="icon-choice" style="width: 175; height: 175;" class="p-2 bg-info">
+                <img src="${imagePath}" alt="${alt}" style="width: 200px; height: 200px;">
+                <br> 
+                ${attribution}</div>`;
+            });
+            document.getElementById("display-choices").innerHTML=htmlText;
+            
+        }
     //disply downloaded default image in flexbox for testing
     TestIConDisplay(){
     let attribution='<a href="https://thenounproject.com/browse/icons/term/cat/" target="_blank" title="Cat Icons">Noun Project</a>';
